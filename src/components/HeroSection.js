e5fa9795ca1ac9779ea3c2ad38d3d9d3e517971d
jaeehan <jaeehan@gmail.com>
2021-05-26 13:09:52 -0700

    deploy my react app to github pages

---
+++ src/components/HeroSection.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../App.css';
+import './HeroSection.css';
+
+function HeroSection() {
+  return (
+    <div className='hero-container'>
+        <div className='hero-container-child'><h1>Hello, I'm J Han</h1></div>
+      
+        <div className='hero-container-child'><p>Hello, I'm J Han</p></div>
+    </div>
+  );
+}
+
+export default HeroSection;
---
+++ src/components/Navbar.js
@@ -0,0 +1,69 @@
+import React, {useState, useEffect} from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+import { Button } from './Button';
+
+function Navbar() {
+    const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
+
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
+
+    const showButton = () => {
+        if (window.innerWidth <= 960) {
+            setButton(false);
+        } else{
+            setButton(true);
+        }
+    };
+
+    //this won't make the email button show when refreshing on minimized window
+    useEffect(() => {
+        showButton();
+    }, []);
+
+    window.addEventListener('resize', showButton);
+
+
+    return (
+        <>
+        <nav className="navbar">
+            <div className="navbar-container">
+                <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
+                    J Han Aye 
+                </Link>
+                <div className='menu-icon' onClick={handleClick}>
+                    <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+                </div>
+                <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                    <li className='nav-item'>
+                        <Link to='/resume' className='nav-links' onClick={closeMobileMenu}>
+                            Resume
+                        </Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link to='/github' className='nav-links' onClick={closeMobileMenu}>
+                            GitHub
+                        </Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link to='/linkedin' className='nav-links' onClick={closeMobileMenu}>
+                            LinkedIn
+                        </Link>
+                    </li>
+                    
+                    <li>
+                        <Link to='/Email' className='nav-links-mobile' onClick = {closeMobileMenu}>
+                            Email
+                        </Link>
+                    </li>
+                </ul>
+                {button && <Button buttonStyle= 'btn--outline'>Email</Button>}
+            </div>
+        </nav>
+        </>
+    )
+}
+
+export default Navbar;

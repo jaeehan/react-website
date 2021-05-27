0602e22592fc1e6b20505b56ebbe4ad5db4819ac
jaeehan <jaeehan@gmail.com>
2021-05-26 23:37:59 -0700

    deploy

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -5,8 +5,7 @@ import './HeroSection.css';
 function HeroSection() {
   return (
     <div className='hero-container'>
-        <div className='hero-container'><h1>Hello, I'm J Han</h1> <p>I.T Student at Kwantlen Polytechnic University</p> </div>
-        <div className='hero-container-child'></div>
+        <div className='hero-container'><h1>Hello, I'm J Han</h1> <p>I.T. Student at Kwantlen Polytechnic University</p> </div>
     </div>
   );
 }

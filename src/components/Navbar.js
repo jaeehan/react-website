0e8b0cb43cf6e09cabd162f5666e0ea38a4d2797
jaeehan <jaeehan@gmail.com>
2021-05-27 21:21:34 -0700

    deploy

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -38,7 +38,7 @@ function Navbar() {
                 </div>
                 <ul className={click ? 'nav-menu active' : 'nav-menu'}>
                     <li className='nav-item'>
-                        <Link to='/resume' className='nav-links' onClick={closeMobileMenu}>
+                        <Link to={'https://github.com/jaeehan'} className='nav-links' onClick={closeMobileMenu}>
                             Resume
                         </Link>
                     </li>
